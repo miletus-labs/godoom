f99b266805b46d04c5c9af85a8d0e9042d591e51
miletus-labs <80297231+miletus-labs@users.noreply.github.com>
2021-07-01 00:50:10 -0700

    Update godoom.go

--- godoom.go
+++ godoom.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/urfave/cli"
 	"github.com/go-gl/gl/v3.3-core/gl"
-	"github.com/go-gl/glfw/v3.1/glfw"
+	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/go-gl/mathgl/mgl32"
 	"image"
 	"image/color"
